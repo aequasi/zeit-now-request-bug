d62e4361c12e3bc07c246db4d12f2573de757dea
Aaron Scherer <aequasi@gmail.com>
2019-06-23 14:11:06 -0700

    Adding local execution

--- test.js
+++ test.js
@@ -1,12 +1,20 @@
 const request = require('request');
 
-export default (req, res) => {
+const handler = (req, res) => {
     request(
         'https://jsonplaceholder.typicode.com/todos/1',
         (err, resp, body) => {
             console.log(err, resp, body);
 
-            res.json(body);
+            if (res) {
+                res.json(body);
+            }
         }
     )
 }
+
+if (!process.env.NOW_REGION) {
+    handler();
+}
+
+module.exports = handler;
