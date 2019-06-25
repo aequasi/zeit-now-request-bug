e22b3a185ae1a2224c5028e4f9c02f39a90c8bca
Aaron Scherer <aequasi@gmail.com>
2019-06-25 09:56:12 -0700

    Updating error reproduction

--- test.js
+++ test.js
@@ -1,16 +1,21 @@
 const request = require('request');
 
 const handler = (req, res) => {
+    /*
     request(
         'https://jsonplaceholder.typicode.com/todos/1',
         (err, resp, body) => {
-            console.log(err, resp, body);
+            if (err) console.log(err);
 
             if (res) {
-                res.json(body);
+                res.status(200).json([JSON.parse(body)]);
+            } else {
+                console.log(body, typeof body); // JSON as string, string
+                console.log(JSON.parse(body));
             }
         }
-    )
+    )*/
+    res.json(undefined);
 }
 
 if (!process.env.NOW_REGION) {
